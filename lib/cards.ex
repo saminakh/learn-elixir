e0c818d5fc61815ed44a49e87e7a29adeb7bb8ea
Samina Khan <skhan191@gmail.com>
2017-03-06 10:42:58 -0500

    Section 2

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,12 +2,12 @@ defmodule Cards do
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-  
+
     for suit <- suits, value <- values do
-      "#{value} of #{suit}" 
-    end  
+      "#{value} of #{suit}"
+    end
   end
-  
+
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
@@ -17,7 +17,23 @@ defmodule Cards do
   end
 
   def deal(deck, hand_size) do
-    { hand, rest_of_deck} = Enum.split(deck, hand_size)
+    {hand, rest_of_deck} = Enum.split(deck, hand_size)
     hand
+  end
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file DNE"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
